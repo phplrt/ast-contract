df4d7330bba59abf5f9e9005b470f634d5dde849
knesmeyanov <k.nesmeyanov@rambler-co.ru>
2019-07-16 21:13:22 +0300

    Typo

--- LeafInterface.php
+++ LeafInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of Phplrt package.
+ * This file is part of phplrt package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,8 +15,7 @@ namespace Phplrt\Contracts\Ast;
 interface LeafInterface extends NodeInterface
 {
     /**
-     * @param int $group
-     * @return string|null
+     * @return string
      */
-    public function getValue(int $group = 0): ?string;
+    public function getValue(): string;
 }
