1cf06ebc9e684fe1836f7ee24e6b0ac2e5c6fc37
SerafimArts <nesk@xakep.ru>
2024-02-10 13:28:41 +0000

    Fix phpunit 10.0 compatibility and upgrade some dependencies

--- tests/CompatibilityTest.php
+++ tests/CompatibilityTest.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace Phplrt\Contracts\Ast\Tests;
 
 use Phplrt\Contracts\Ast\NodeInterface;
-use PHPUnit\Framework\Attributes\Group;
 
 /**
  * Note: Changing the behavior of these tests is allowed ONLY when updating
  *       a MAJOR version of the package.
  */
-#[Group('phplrt/ast-contracts')]
 class CompatibilityTest extends TestCase
 {
     public function testNodeCompatibility(): void
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Phplrt\Contracts\Ast\Tests;
 
-use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
-#[Group('phplrt/ast-contracts')]
 abstract class TestCase extends BaseTestCase {}
